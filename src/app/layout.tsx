9a71e5f425b44c7e02dcf67557f40411983d15ac
Tristan A. <tristanagraa@gmail.com>
2024-05-25 01:39:59 +0700

    feat: suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
-        <Toaster />
+        <Suspense>
+          {children}
+          <Toaster />
+        </Suspense>
       </body>
     </html>
   );
